0ee6e9e471b30e5b67b62b74ce0aee826312fe05
Hristo Papanov <hristopapanov13@gmail.com>
2022-09-13 20:42:38 +0300

    Added LAB tasks

---
+++ 01. Stacks and Queues/LAB/03.SimpleCalculator/03.SimpleCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            Stack<int> indexes = new Stack<int>();
+
+            for(int i=0; i<expression.Length; i++)
+            {
+                if (expression[i]!='+' && expression[i]!='-')
+                {
+                    indexer
+                }
+            }
+        }
+    }
+}
+
---
+++ 01. Stacks and Queues/LAB/05.PrintEvenNumbers/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> nums = new Queue<int>();
+            string[] numbers = Console.ReadLine().Split();
+            foreach(var x in numbers)
+            {
+                if(int.Parse(x)%2==0)
+                {
+                    nums.Enqueue(int.Parse(x));
+                }
+            }
+            Console.WriteLine(String.Join(", ", nums));
+        }
+    }
+}
+
