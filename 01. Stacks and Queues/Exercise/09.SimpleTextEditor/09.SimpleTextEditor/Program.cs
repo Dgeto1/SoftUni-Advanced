6705bf5d8df8ceb450d0511da2254b5793834751
Hristo Papanov <hristopapanov13@gmail.com>
2022-09-15 20:47:13 +0300

    Ex

---
+++ 01. Stacks and Queues/Exercise/09.SimpleTextEditor/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09.SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOperations = int.Parse(Console.ReadLine());
+            Stack<string> text = new Stack<string>();
+            for(int i=1; i<=numberOperations;i++)
+            {
+                string[] segments = Console.ReadLine().Split();
+                int command = int.Parse(segments[0]);
+                if(command==1)
+                {
+                    text.Push(segments[1]);
+                }
+                else if(command==2)
+                {
+                    int count = int.Parse(segments[1]);
+                    while(count>0)
+                    {
+                        text.Pop();
+                        count--;
+                    }
+                }
+                else if(command==3)
+                {
+                    int index = int.Parse(segments[1]);
+                    int currentIndex = 1;
+                    
+                }
+                else if(command==4)
+                {
+                    text.Pop();
+                }
+            }
+        }
+    }
+}
+
