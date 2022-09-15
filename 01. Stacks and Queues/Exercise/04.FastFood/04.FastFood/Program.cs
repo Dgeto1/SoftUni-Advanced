1a9a904373c616d6457102f34273516dd929553f
Hristo Papanov <hristopapanov13@gmail.com>
2022-09-15 19:54:22 +0300

    Ex

---
+++ 01. Stacks and Queues/Exercise/04.FastFood/04.FastFood/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityFood = int.Parse(Console.ReadLine());
+            string[] sequence = Console.ReadLine().Split();
+            Queue<int> orders = new Queue<int>();
+            for(int i=0;i<sequence.Length;i++)
+            {
+                orders.Enqueue(int.Parse(sequence[i]));
+            }
+            Queue<int> maxOrder = new Queue<int>(orders);
+            int maxOrd = maxOrder.Dequeue();
+            while(maxOrder.Count>0)
+            {
+                int order = maxOrder.Dequeue();
+                if(order>maxOrd)
+                {
+                    maxOrd = order;
+                }
+            }
+            Console.WriteLine(maxOrd);
+            while(orders.Count>0)
+            {
+                int currentOrder = orders.Peek();
+
+                if (currentOrder<quantityFood)
+                {
+                    orders.Dequeue();
+                    quantityFood -= currentOrder;
+                    if(quantityFood<=0)
+                    {
+                        Console.WriteLine($"Orders left: {String.Join(" ", orders)}");
+                        return;
+                    }
+                }
+                
+            }
+            if(orders.Count==0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
+
