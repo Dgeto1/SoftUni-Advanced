fb14b6cc3a54177780919fe6731059e4f95ad870
Hristo Papanov <hristopapanov13@gmail.com>
2022-09-16 11:40:50 +0300

    ex

---
+++ 01. Stacks and Queues/Exercise/07.TruckTour/07.TruckTour/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i=1; i<=n; i++)
+            {
+                string[] segments = Console.ReadLine().Split();
+                int amountPetrol = int.Parse(segments[0]);
+                int distanceBetweenPump = int.Parse(segments[1]);
+            }
+        }
+    }
+}
+
