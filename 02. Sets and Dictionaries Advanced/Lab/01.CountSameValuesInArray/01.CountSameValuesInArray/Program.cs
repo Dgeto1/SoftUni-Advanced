570303c413253bc4c863ecf3ee91435361f12699
Hristo Papanov <hristopapanov13@gmail.com>
2022-09-26 19:11:51 +0300

    ex

---
+++ 02. Sets and Dictionaries Advanced/Lab/01.CountSameValuesInArray/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01.CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = Console.ReadLine().Split().(double.Parse());
+        }
+    }
+}
+
---
+++ 02.Multidimentional Arrays/Lab/02.SumMatrixColumns/02.SumMatrixColumns/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sizes = Console.ReadLine().Split(", ");
+            int rows = int.Parse(sizes[0]);
+            int columns = int.Parse(sizes[1]);
+
+            int[,] matrix = new int[rows, columns];
+
+            for(int i=0; i<rows; i++)
+            {
+                int[] values = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for(int j=0; j<columns;j++)
+                {
+                    matrix[i, j] = values[j];
+                }
+            }
+            for(int i=0; i<matrix.GetLength(1); i++)
+            {
+                int sum = 0;
+                for (int j=0; j<matrix.GetLength(0); j++)
+                {
+                    sum += matrix[j, i];
+                }
+                Console.WriteLine(sum);
+            }    
+        }
+    }
+}
+
