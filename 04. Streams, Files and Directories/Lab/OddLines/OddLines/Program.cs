3f98d5e12b04cfddc1ab4ea05f58cff1633aae52
Hristo Papanov <hristopapanov13@gmail.com>
2022-09-26 22:00:43 +0300

    Ex

---
+++ 04. Streams, Files and Directories/Lab/OddLines/OddLines/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int row = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (row % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        row++;
+                    }
+                }
+            }
+        }
+    }
+}
+
