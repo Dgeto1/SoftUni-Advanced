029180540bce63106a51ede33745a327929c4ea0
Hristo Papanov <hristopapanov13@gmail.com>
2022-09-27 13:54:40 +0300

    ex

---
+++ 02. Sets and Dictionaries Advanced/Lab/04.ProductShop/04.ProductShop/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, string> shopProducts = new SortedDictionary<string, string>();
+            Dictionary<string, double> productsPrice = new Dictionary<string, double>();
+            string command;
+            while((command = Console.ReadLine())!="Revision")
+            {
+                string[] segments = command.Split(", ");
+                string shopName = segments[0];
+                string product = segments[1];
+                double price = double.Parse(segments[2]);
+                if (!shopProducts.ContainsKey(product))
+                {
+                    shopProducts.Add(product, shopName);
+                    productsPrice.Add(product, price);
+                }
+                else
+                {
+                    shopProducts[product] = shopName;
+                    productsPrice.Add(product, price);
+                }
+            }
+            foreach(var x in shopProducts)
+            {
+                foreach(var z in shopProducts)
+                {
+                    if(x.Value==z.Value+1)
+                    {
+                        Console.WriteLine($"{x.Value}->");
+                    }
+                }
+                 
+                 foreach (var y in productsPrice)
+                 {
+                    if (x.Key == y.Key)
+                    {
+                        Console.WriteLine($"Product: {x.Key}, Price: {y.Value}");
+                    }
+                 } 
+            }
+            Console.Read();
+        }
+    }
+}
+
---
+++ 02. Sets and Dictionaries Advanced/Lab/05.CitiesByContinentAndCountry/05.CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.CitiesByContinentAndCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> citiesContinets = new Dictionary<string, Dictionary<string, string>>();
+            int n = int.Parse(Console.ReadLine());
+            for(int i=0; i<n; i++)
+            {
+                string[] segments = Console.ReadLine().Split();
+                string continent = segments[0];
+                string country = segments[1];
+                string city = segments[2];
+
+                if(!citiesContinets.ContainsKey(continent))
+                {
+                    citiesContinets.Add(continent, new Dictionary<string, string>());
+                    citiesContinets[continent].Add(country, city);
+                }
+                else if(citiesContinets.ContainsKey(continent) && citiesContinets.ContainsValue(country))
+            }
+        }
+    }
+}
+
