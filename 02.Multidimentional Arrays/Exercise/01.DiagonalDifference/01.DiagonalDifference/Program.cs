ded7f27b86957faaa0b7c0994835bc36ad293acd
Hristo Papanov <hristopapanov13@gmail.com>
2022-09-28 17:19:22 +0300

    ex

---
+++ 02.Multidimentional Arrays/Exercise/01.DiagonalDifference/01.DiagonalDifference/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for(int i=0; i<n; i++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = numbers[j];
+                }
+            }
+            int primarySum = 0;
+            int secondarySum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        primarySum += matrix[i, j];
+                    }
+                    if (i + j == n - 1)
+                    {
+                        secondarySum += matrix[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(Math.Abs(primarySum-secondarySum));
+        }
+    }
+}
+
---
+++ 02.Multidimentional Arrays/Exercise/02.SquaresInMatrix/02.SquaresInMatrix/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            char[,] matrix = new char[sizes[0], sizes[1]];
+
+            for(int i=0; i < sizes[0]; i++)
+            {
+                char[] segments = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for(int j=0; j < sizes[1]; j++)
+                {
+                    matrix[i, j] = segments[j];
+                }
+            }
+            int counter = 0;
+
+            for(int i=0; i < matrix.GetLength(0)-1; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1)-1; j++)
+                {
+                    if (matrix[i, j] == matrix[i, j+1] && matrix[i+1, j] == matrix[i + 1, j + 1] && matrix[i, j] == matrix[i + 1, j])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}   
+
