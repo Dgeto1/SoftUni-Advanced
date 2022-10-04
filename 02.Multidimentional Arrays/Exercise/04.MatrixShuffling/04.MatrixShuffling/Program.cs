d6a228fd6477ae4b1e7d173b7d88c503734f7c46
Hristo Papanov <hristopapanov13@gmail.com>
2022-10-04 20:16:42 +0300

    ex

--- 02.Multidimentional Arrays/Exercise/04.MatrixShuffling/04.MatrixShuffling/Program.cs
+++ 02.Multidimentional Arrays/Exercise/04.MatrixShuffling/04.MatrixShuffling/Program.cs
@@ -19,41 +19,42 @@ namespace _04.MatrixShuffling
                 }
             }
             string commands;
-            bool isValid = true;
+
             while ((commands = Console.ReadLine()) != "END")
             {
                 string[] segments = commands.Split();
-                if (segments[0] != "swap" || int.Parse(segments[1]) >= matrix.GetLength(0) || int.Parse(segments[2]) >= matrix.GetLength(0) || int.Parse(segments[3]) >= matrix.GetLength(1) || int.Parse(segments[4]) >= matrix.GetLength(1))
+                if (segments[0] != "swap")
                 {
                     Console.WriteLine("Invalid input!");
-                    isValid = false;
                 }
-                
-
-                
-                else
+                else if (segments.Length == 5)
                 {
-                    int row1 = int.Parse(segments[1]);
-                    int col1 = int.Parse(segments[2]);
-                    int row2 = int.Parse(segments[3]);
-                    int col2 = int.Parse(segments[4]);
-                    string swap = matrix[row1, col1];
-                    matrix[row1, col1] = matrix[row2, col2];
-                    matrix[row2, col2] = swap;
-                    isValid = true;
-
-
-                }
-                if (isValid)
-                {
-                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    if (segments[0] != "swap" || int.Parse(segments[1]) >= matrix.GetLength(0) || int.Parse(segments[2]) >= matrix.GetLength(1) || int.Parse(segments[3]) >= matrix.GetLength(0) || int.Parse(segments[4]) >= matrix.GetLength(1))
                     {
-                        for (int j = 0; j < matrix.GetLength(1); j++)
-                        {
-                            Console.Write(String.Join(" ", matrix[i, j] + " "));
-                        }
-                        Console.WriteLine();
+                        Console.WriteLine("Invalid input!");
                     }
+
+                    else
+                    {
+                        int row1 = int.Parse(segments[1]);
+                        int col1 = int.Parse(segments[2]);
+                        int row2 = int.Parse(segments[3]);
+                        int col2 = int.Parse(segments[4]);
+                        string swap = matrix[row1, col1];
+                        matrix[row1, col1] = matrix[row2, col2];
+                        matrix[row2, col2] = swap;
+                        for (int i = 0; i < matrix.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < matrix.GetLength(1); j++)
+                            {
+                                Console.Write(String.Join(" ", matrix[i, j] + " "));
+                            }
+                            Console.WriteLine();
+
+                        }
+                    }
+
+
                 }
             }
         }
---
+++ 02.Multidimentional Arrays/Exercise/05.SnakeMoves/05.SnakeMoves/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string[,] matrix = new string[sizes[0], sizes[1]];
+
+            string snake = Console.ReadLine();
+
+            int counter = 0;
+            for(int i=0; i<matrix.GetLength(0); i++)
+            {
+                if(i%2!=0)
+                {
+                    for (int j = matrix.GetLength(1); j >= 0; j--)
+                    {
+                        if(counter == snake.Length)
+                        {
+                            counter = 0;
+                        }
+                            matrix[i, j] = snake[counter].ToString();
+                            counter++;
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j <matrix.GetLength(1); j++)
+                    {
+                        if (counter == snake.Length)
+                        {
+                            counter = 0;
+                        }
+                        matrix[i, j] = snake[counter].ToString();
+                        counter++;
+                    }
+                }
+            }
+            for(int i=0; i<matrix.GetLength(0); i++)
+            {
+                for(int j=0; j<matrix.GetLength(1); j++)
+                {
+                    Console.Write(String.Join(" ", matrix[i,j]));
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
+
