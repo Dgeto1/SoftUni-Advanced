46fa82b453b1e35f4d75a2bf172b2914d8837038
Hristo Papanov <hristopapanov13@gmail.com>
2022-10-04 13:22:34 +0300

    Ex

---
+++ 02.Multidimentional Arrays/Exercise/04.MatrixShuffling/04.MatrixShuffling/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.MatrixShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string[,] matrix = new string[sizes[0], sizes[1]];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string[] values = Console.ReadLine().Split();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = values[j];
+                }
+            }
+            string commands;
+            bool isValid = true;
+            while ((commands = Console.ReadLine()) != "END")
+            {
+                string[] segments = commands.Split();
+                if (segments[0] != "swap" || int.Parse(segments[1]) >= matrix.GetLength(0) || int.Parse(segments[2]) >= matrix.GetLength(0) || int.Parse(segments[3]) >= matrix.GetLength(1) || int.Parse(segments[4]) >= matrix.GetLength(1))
+                {
+                    Console.WriteLine("Invalid input!");
+                    isValid = false;
+                }
+                
+
+                
+                else
+                {
+                    int row1 = int.Parse(segments[1]);
+                    int col1 = int.Parse(segments[2]);
+                    int row2 = int.Parse(segments[3]);
+                    int col2 = int.Parse(segments[4]);
+                    string swap = matrix[row1, col1];
+                    matrix[row1, col1] = matrix[row2, col2];
+                    matrix[row2, col2] = swap;
+                    isValid = true;
+
+
+                }
+                if (isValid)
+                {
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < matrix.GetLength(1); j++)
+                        {
+                            Console.Write(String.Join(" ", matrix[i, j] + " "));
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
