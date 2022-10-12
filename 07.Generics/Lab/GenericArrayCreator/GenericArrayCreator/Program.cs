53a56250c1a048f636a151ca489be8314db9bf72
Hristo Papanov <hristopapanov13@gmail.com>
2022-10-12 15:45:52 +0300

    ex

---
+++ 07.Generics/Lab/GenericArrayCreator/GenericArrayCreator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = ArrayCreator.Create(10, 34);
+            Console.Write(String.Join(",", numbers));
+        }
+    }
+}
+
