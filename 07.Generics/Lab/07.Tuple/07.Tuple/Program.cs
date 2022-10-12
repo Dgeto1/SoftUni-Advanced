1c49f8a56cd61fd69b15024e9cda552855186c6e
Hristo Papanov <hristopapanov13@gmail.com>
2022-10-12 17:23:54 +0300

    ex

---
+++ 07.Generics/Lab/07.Tuple/07.Tuple/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstLine = Console.ReadLine().Split();
+            string[] secondLine = Console.ReadLine().Split();
+            string[] thirdLine = Console.ReadLine().Split();
+
+            Tuple<string, string> nameAdress = new Tuple<string, string>
+            {
+                Item1 = $"{firstLine[0]} {firstLine[1]}",
+                Item2 = $"{firstLine[2]}"
+            };
+
+            Tuple<string, int> personDrink = new Tuple<string, int>
+            {
+                Item1 = $"{secondLine[0]}",
+                Item2 = int.Parse(secondLine[1])
+            };
+
+            Tuple<int, double> numbers = new Tuple<int, double>
+            {
+                Item1 = int.Parse(thirdLine[0]),
+                Item2 = double.Parse(thirdLine[1])
+            };
+            Console.WriteLine(nameAdress.ToString());
+            Console.WriteLine(personDrink.ToString());
+            Console.WriteLine(numbers.ToString());
+            Console.Read();
+        }
+    }
+}
+
---
+++ 07.Generics/Lab/07.Tuple/07.Tuple/Tuple.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace _07.Tuple
+{
+	public class Tuple<T1, T2>
+	{
+		public T1 Item1 { get; set; }
+		public T2 Item2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2}";
+        }
+    }
+}
+
