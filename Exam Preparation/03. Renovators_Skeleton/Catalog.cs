3dbc270e2c786ca83f43934544e0740bdb3b3333
Hristo Papanov <hristopapanov13@gmail.com>
2022-10-16 10:37:26 +0300

    ex

---
+++ Exam Preparation/03. Renovators_Skeleton/Catalog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Catalog
+    {
+        private List<Renovator> renovators;
+        private string name;
+        private int neededRenovators;
+        private string project;
+
+        public Catalog(string name, int neededRenovators, string project)
+        {
+            Renovators = new List<Renovator>();
+            Name = name;
+            NeededRenovators = neededRenovators;
+            Project = project;
+        }
+
+        public List<Renovator> Renovators { get; set; }
+        public string Name { get; set; }
+        public int NeededRenovators { get; set; }
+        public string Project { get; set; }
+
+        public int Count()
+        {
+            return renovators.Count();
+        }
+
+        public string AddRenovator(Renovator renovator)
+        {
+            if(renovator.Name == null)
+            {
+                Console.WriteLine("Invalid renovator's information.");
+            }
+            if(renovator.)
+        }
+    }
+}
---
+++ Exam Preparation/03. Renovators_Skeleton/Renovator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Renovator
+    {
+        private string name;
+        private string type;
+        private double rate;
+        private int days;
+        private bool hired;
+
+        public Renovator(string name, string type, double rate, int days)
+        {
+            Name = name;
+            Type = type;
+            Rate = rate;
+            Days = days;
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Rate { get; set; }
+        public int Days { get; set; }
+        public bool Hired { get; set; }
+
+        public override string ToString()
+        {
+            return $"-Renovator: {name}\n--Speciality: {type}\n--Rate per day: {rate} BGN";
+        }
+    }
+}
