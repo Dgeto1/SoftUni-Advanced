f7f151dc7e685dd0947023630ad0fd33cdcc2ebc
Hristo Papanov <hristopapanov13@gmail.com>
2022-10-21 21:58:01 +0300

    ex

---
+++ Exam Preparation/02.BeaverAtWork/02.BeaverAtWork/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02.BeaverAtWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<char> collectedBranches = new List<char>();
+            char[,] matrix = new char[n, n];
+
+            int colB = 0, rowB = 0;
+
+            for(int i=0; i<n; i++)
+            {
+                char[] segments = Console.ReadLine().Split().Select(char.Parse).ToArray();
+                for(int j=0; j<n; j++)
+                {
+                    matrix[i, j] = segments[j];
+                    if (matrix[i,j] == 'B')
+                    {
+                        colB = i;
+                        rowB = j;
+                    }
+                }
+            }
+
+            string command;
+            while((command = Console.ReadLine())!="end")
+            {
+                int previewCol = colB, previewRow = rowB;
+                
+                switch (command)
+                {
+                    case "up":
+                        colB--;
+                        if (colB < 0 || colB >= n || rowB<0 || rowB>=n)
+                        {
+                            if (collectedBranches.Count > 0)
+                            {
+                                collectedBranches.Remove(collectedBranches[collectedBranches.Count()-1]);
+                            }
+                        }
+                        else
+                        {
+                            matrix[previewCol, previewRow] = '-';
+
+                            if (char.IsLower(matrix[colB, rowB]))
+                            {
+                                collectedBranches.Add(matrix[colB, rowB]);
+                            }
+                            else if (matrix[colB, rowB] == 'F')
+                            {
+                                if (colB > 0)
+                                {
+                                    matrix[colB, rowB] = '-';
+                                    if (char.IsLower(matrix[0, rowB]))
+                                    {
+                                        collectedBranches.Add(matrix[0, rowB]);
+                                    }
+                                    matrix[0, rowB] = 'B';
+                                    colB = 0;
+                                }
+                                else
+                                {
+                                    matrix[colB, rowB] = '-';
+                                    if (char.IsLower(matrix[n-1, rowB]))
+                                    {
+                                        collectedBranches.Add(matrix[n-1, rowB]);
+                                    }
+                                    matrix[n-1, rowB] = 'B';
+                                    colB = n-1;
+                                }
+                            }
+                        }
+                        break;
+                    case "down":
+                        colB++;
+                        if (colB < 0 || colB >= n || rowB < 0 || rowB >= n)
+                        {
+                            if (collectedBranches.Count > 0)
+                            {
+                                collectedBranches.Remove(collectedBranches[collectedBranches.Count()-1]);
+                            }
+                        }
+                        else
+                        {
+
+
+                            matrix[previewCol, previewRow] = '-';
+                            if (char.IsLower(matrix[colB, rowB]))
+                            {
+                                collectedBranches.Add(matrix[colB, rowB]);
+                            }
+                            else if (matrix[colB, rowB] == 'F')
+                            {
+                                if (colB < n)
+                                {
+                                    matrix[colB, rowB] = '-';
+                                    if (char.IsLower(matrix[n, rowB]))
+                                    {
+                                        collectedBranches.Add(matrix[n-1, rowB]);
+                                    }
+                                    matrix[n-1, rowB] = 'B';
+                                    colB = n-1;
+                                }
+                                else
+                                {
+                                    matrix[colB, rowB] = '-';
+                                    if (char.IsLower(matrix[n-1, rowB]))
+                                    {
+                                        collectedBranches.Add(matrix[n-1, rowB]);
+                                    }
+                                    matrix[n-1, rowB] = 'B';
+                                    colB = n-1;
+                                }
+                            }
+                        }
+                        break;
+                    case "right":
+                        rowB++;
+                        if (colB < 0 || colB >= n || rowB < 0 || rowB >= n)
+                        {
+                            if (collectedBranches.Count > 0)
+                            {
+                                collectedBranches.Remove(collectedBranches[collectedBranches.Count()-1]);
+                            }
+                        }
+                        else
+                        {
+                            matrix[previewCol, previewRow] = '-';
+                            if (char.IsLower(matrix[colB, rowB]))
+                            {
+                                collectedBranches.Add(matrix[colB, rowB]);
+                            }
+                            else if (matrix[colB, rowB] == 'F')
+                            {
+                                if (rowB < n)
+                                {
+                                    matrix[colB, rowB] = '-';
+                                    if (char.IsLower(matrix[colB, n-1]))
+                                    {
+                                        collectedBranches.Add(matrix[colB, n-1]);
+                                    }
+                                    matrix[colB, n-1] = 'B';
+                                    rowB = n-1;
+                                }
+                            }
+                        }
+                        break;
+                    case "left":
+                        rowB--;
+                        if (colB < 0 || colB >= n || rowB < 0 || rowB >= n)
+                        {
+                            if (collectedBranches.Count > 0)
+                            {
+                                collectedBranches.Remove(collectedBranches[collectedBranches.Count()-1]);
+                            }
+                        }
+                        else
+                        {
+                            matrix[previewCol, previewRow] = '-';
+                            if (char.IsLower(matrix[colB, rowB]))
+                            {
+                                collectedBranches.Add(matrix[colB, rowB]);
+                            }
+                            else if (matrix[colB, rowB] == 'F')
+                            {
+                                if (rowB > 0)
+                                {
+                                    matrix[colB, rowB] = '-';
+                                    if (char.IsLower(matrix[colB, 0]))
+                                    {
+                                        collectedBranches.Add(matrix[colB, 0]);
+                                    }
+                                    matrix[colB, 0] = 'B';
+                                    rowB = 0;
+                                }
+                            }
+                        }
+                        break;
+                }
+                
+            }
+            bool isCollected = true;
+            int notCollected = 0;
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    if (char.IsLower(matrix[i,j]))
+                    {
+                        isCollected = false;
+                        notCollected++;
+                    }
+                }
+            }
+            if (isCollected)
+            {
+                Console.WriteLine($"The Beaver successfully collect {collectedBranches.Count()} wood branches: {String.Join(", ", collectedBranches)}.");
+            }
+            else
+            {
+                Console.WriteLine($"The Beaver failed to collect every wood branch. There are {notCollected} branches left.");
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
+
---
+++ Exam Preparation/Drones/Drones/Airfield.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Drones
+{
+    public class Airfield
+    {
+        private List<Drone> drones;
+        private string name;
+        private int capacity;
+        private double landingStrip;
+
+        public Airfield(string name, int capacity, double landingStrip)
+        {
+            drones = new List<Drone>();
+            Name = name;
+            Capacity = capacity;
+            LandingStrip = landingStrip;
+        }
+
+        public List<Drone> Drones
+        {
+            get { return drones; }
+            set { drones = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+        public double LandingStrip
+        {
+            get { return landingStrip; }
+            set { landingStrip = value; }
+        }
+
+        public int Count
+        {
+            get { return drones.Count(); }
+        }
+
+        public string AddDrone(Drone drone)
+        {
+            if(string.IsNullOrEmpty(drone.Name) || string.IsNullOrEmpty(drone.Brand) || drone.Range<5 || drone.Range>15)
+            {
+                return "Invalid drone.";
+            }
+            if(drones.Count()==Capacity)
+            {
+                return "Airfield is full.";
+            }
+
+            drones.Add(drone);
+            return $"Successfully added {drone.Name} to the airfield.";
+        }
+
+        public bool RemoveDrone(string name)
+        {
+            Drone drone = drones.FirstOrDefault(x => x.Name == name);
+
+            return drones.Remove(drone);
+        }
+
+        public int RemoveDroneByBrand(string brand)
+        {
+            List<Drone> dr = drones.Where(x => x.Brand != brand).ToList();
+            int count = drones.Count() - dr.Count();
+            drones = dr;
+
+            return count;
+        }
+
+        public Drone FlyDrone(string name)
+        {
+            Drone drone = drones.FirstOrDefault(x => x.Name == name);
+            if(drone!=null)
+            {
+                drone.Available = false;
+            }
+
+            return drone;
+        }
+
+        public List<Drone> FlyDronesByRange(int range)
+        {
+            return drones.Where(x => x.Range >= range).ToList();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Drones available at {Name}:");
+
+            List<Drone> available = drones.Where(x => x.Available == true).ToList();
+            foreach(Drone x in available)
+            {
+                sb.Append($"{x}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
